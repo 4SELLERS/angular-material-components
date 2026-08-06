7c7a647607e96cd2259f4749c28c566ae1d3c765
Michael Koch <michael.koch@salesware.de>
2026-08-06 09:49:01 +0200

    add provider

--- projects/moment-adapter/src/lib/moment-adapter.module.ts
+++ projects/moment-adapter/src/lib/moment-adapter.module.ts
@@ -1,9 +1,9 @@
 
 
-import { NgxMatDateAdapter, NGX_MAT_DATE_FORMATS } from '@salesware/angular-material-components-datetime-picker';
-import { NgModule } from '@angular/core';
+import { NgxMatDateAdapter, NGX_MAT_DATE_FORMATS, NgxMatDateFormats } from '@salesware/angular-material-components-datetime-picker';
+import { NgModule, Provider } from '@angular/core';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
-import { NgxMatMomentAdapter, NGX_MAT_MOMENT_DATE_ADAPTER_OPTIONS } from './moment-adapter';
+import { NgxMatMomentAdapter, NGX_MAT_MOMENT_DATE_ADAPTER_OPTIONS, NgxMatMomentDateAdapterOptions } from './moment-adapter';
 import { NGX_MAT_MOMENT_FORMATS } from './moment-formats';
 
 @NgModule({
@@ -23,3 +23,23 @@ export class NgxMomentDateModule { }
   providers: [{ provide: NGX_MAT_DATE_FORMATS, useValue: NGX_MAT_MOMENT_FORMATS }],
 })
 export class NgxMatMomentModule { }
+
+export function provideNgxMatMomentDateAdapter(
+  formats: NgxMatDateFormats = NGX_MAT_MOMENT_FORMATS,
+  options?: NgxMatMomentDateAdapterOptions
+): Provider[] {
+  const providers: Provider[] = [
+    {
+      provide: NgxMatDateAdapter,
+      useClass: NgxMatMomentAdapter,
+      deps: [MAT_DATE_LOCALE, NGX_MAT_MOMENT_DATE_ADAPTER_OPTIONS]
+    },
+    { provide: NGX_MAT_DATE_FORMATS, useValue: formats }
+  ];
+
+  if (options) {
+    providers.push({ provide: NGX_MAT_MOMENT_DATE_ADAPTER_OPTIONS, useValue: options });
+  }
+
+  return providers;
+}
